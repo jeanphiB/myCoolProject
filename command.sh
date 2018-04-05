67d34085b41a3683c7eceebcd55258bdb939244a
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-04-05 15:52:53 +0200

    a more cool command.sh

--- command.sh
+++ command.sh
@@ -1,2 +1,9 @@
 pwd
 ls -l
+echo "0622334455">myFile.txt
+echo "07 89 70 44 56">>myFile.txt
+echo "+33625125987">>myFile.txt
+echo "0033722896744">>myFile.txt
+echo "03-20-44-99-26">>myFile.txt
+echo "03.20.44.99.26">>myFile.txt
+cat myFile.txt | grep -E "^((\+|00)[0-9]{3}|[0-9]{2})([\. -]?[0-9]{2}){4}"
